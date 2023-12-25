2c976f618cfaf18f06dcc93e3fcd9840752e13eb
Akari <33871744+JimmyLing233@users.noreply.github.com>
2023-12-25 22:00:07 +0800

    2.1更新
    添加了中文维百语言变体重定向

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         中文维百谷歌重定向
-// @version      1.0
-// @description  Redirects from zh.m.wikipedia.org to zh.wikipedia.org
+// @version      2.1
+// @description  Redirects Wikipedia
 // @namespace    https://github.com/JimmyLing233/redirect-zhwiki-google
 // @author       Akari
 // @include      *://zh.m.wikipedia.org/*
+// @include      *://zh.wikipedia.org/*
 // @license      MIT
 // @grant        none
 // @inject-into  auto
@@ -12,10 +13,22 @@
 // ==/UserScript==
 
 var url = new URL(location.href);
-var re = /zh.m.wikipedia\.org\/*/;
-if(re.test(url.hostname))// 二次正则匹配，避免误判
-{
-    window.stop();// 停止加载当前页面
-    url.hostname = url.hostname.replace(re, 'zh.wikipedia.com');// 替换域名
-    location.assign(url.href);// 从当前页面会转为新页面
+var hasRedirected = false; // 添加一个标志来检查是否已经进行了重定向
+
+// 第一个重定向条件
+var re = /zh\.m\.wikipedia\.org\/*/;
+if (re.test(url.hostname) && !hasRedirected) {
+    url.hostname = url.hostname.replace(re, 'zh.wikipedia.com');
+    location.assign(url.href);
+    hasRedirected = true; // 设置重定向标志为true
+}
+
+var newUrl = new URL(location.href);
+// 第二个重定向条件
+var newRe = /zh\.wikipedia\.org\/zh-[a-z]{2,4}\/*/;
+if (newRe.test(newUrl.hostname) && !hasRedirected) {
+    newUrl.hostname = newUrl.hostname.replace(newRe, 'zh.wikipedia.com');
+    newUrl.pathname = '/wiki' + newUrl.pathname; // 添加/wiki/到路径前面
+    location.assign(newUrl.href);
+    hasRedirected = true; // 设置重定向标志为true
 }
